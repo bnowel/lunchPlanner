6de3d49a4fd6fdd8ac43cbd473218e5d0c7d4e53
Bernie <bnowel@yahoo.com>
2013-01-29 01:56:06 -0500

    Added some default yelp data and knockout bindings

--- static/js/client.js
+++ static/js/client.js
@@ -1,8 +1,31 @@
+//http://learn.knockoutjs.com/#/?tutorial=collections
+function PageViewModel() {
+    var self = this;
+    
+    self.businesses = ko.observableArray([
+        {
+            name: "Jaffa", 
+            display_phone: "80566666", 
+            rating_img_url: "http://s3-media2.ak.yelpcdn.com/assets/2/www/img/99493c12711e/ico/stars/v1/stars_4_half.png"
+        },
+        {
+            name: "Burger King", 
+            display_phone: "22222", 
+            rating_img_url: "http://s3-media2.ak.yelpcdn.com/assets/2/www/img/99493c12711e/ico/stars/v1/stars_4_half.png"
+        }
+    ]);
+}
+
 $(function() {
+    // Knockout stuff
+    ko.applyBindings(new PageViewModel());    
+    
+    
     // Page stuff
     $('#search').on('click', function() {
         console.log('searching...');
        $.get('search', {term: $('#terms').val()}, function(data) {
+           PageViewModel.businesses = data.businesses;
            console.log(data);
        });
     });
